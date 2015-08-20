727927845dfff9f154587c4557366ae381fea7cf
曹逸君 <classone2010@gmail.com>
2015-08-20 14:09:37 +0800

    click radio trigger ngmodel change

--- vendor/content.js
+++ vendor/content.js
@@ -2,8 +2,7 @@ $(document).ready(function () {
 	var switchers = {
 		"init": function(){
 			// set default values
-			set_settings([["#is-always-notify-in-bar",[false]],[".notifytype_check",[true,true,true]],["#pushtype_select",["notify"]],["#is-always-notify-in-bar",[false]],["#notifytype_input",["7"]],["#pushTitle",[""]],["#pushContent",[""]],["select[name=notifyEffectType]",["1"]],["input[name=scheduledTime]",["0","0"]],["input[name=push-time-type]",[true,false,true,false]],["#flow-control-checkbox",[true]],["input[name=notifyId]",[""]],["input[name=apitype_select]",[false,false,true,false,false,false,true,false,false,false,false,false]]]);
-
+			set_default();
 			$('body > div.main.myitems > div > div.android-container.push-container.ng-scope > form > div.control-group.mt26 > div')
 			.append("<button class='btn btn-large btn-success'>全部推送</button>");
 			$('body > div.main.myitems > div > div.android-container.push-container.ng-scope > form > div.control-group.mt26 > div > button.btn.btn-large.btn-success')
@@ -34,7 +33,7 @@ $(document).ready(function () {
 		"next": function(){
 			var remains = $('#min-con > ul > li > a').filter(function(i, elem){
 				var packageid = elem.href.match(/[^/]+/g)[6];
-				return localStorage['sended'].indexOf(packageid) == -1; // && "com.xiaomi.mipushdemo/com.giu.rekoo.luta.rk".indexOf(packageid) > -1;
+				return localStorage['sended'].indexOf(packageid) == -1; // && "com.xiaomi.mipushdemo".indexOf(packageid) > -1;
 			});
 			if (remains.length){
 				localStorage['action'] = 'send';
@@ -45,6 +44,9 @@ $(document).ready(function () {
 			}
 		}
 	};
+	function set_default(){
+		set_settings([["#is-always-notify-in-bar",[["0",false]]],[".notifytype_check",[["u",true],["undefined",true],["u",true]]],["#pushtype_select",["notify"]],["#is-always-notify-in-bar",[["0",false]]],["#notifytype_input",["NaN"]],["#pushTitle",[""]],["#pushContent",[""]],["select[name=notifyEffectType]",["1"]],["input[name=push-time-type]",[["u",false],["undefined",false],["u",true],["undefined",false]]],[".expires-datepicker-input",["","2015-09-03 13:41"]],["#flow-control-checkbox",[["u",true]]],["input[name=notifyId]",["100"]],["input[name=apitype_select]",[["u",false],["undefined",false],["u",true],["undefined",false],["u",false],["undefined",false],["u",true],["undefined",false],["u",false],["undefined",false],["u",false],["undefined",false]]]]);
+	}
 	function get_settings(){
 		var elems = [	"#is-always-notify-in-bar",
 						".notifytype_check",
@@ -64,7 +66,7 @@ $(document).ready(function () {
 		return elems.map(function(name){
 			return [name, $(name).map(function(i, item){
 				if ("radio/checkbox".indexOf(item.type) > -1) {
-					return item.checked;
+					return [[item.value, item.checked]];
 				}else{
 					return item.value;
 				}
@@ -72,11 +74,14 @@ $(document).ready(function () {
 		});
 	}
 	function set_settings(settings){
-		console.log(settings);
 		settings.map(function(pair, index){
 			$(pair[0]).map(function(i, elem){
 				if ("radio/checkbox".indexOf(elem.type) > -1) {
-					elem.checked = pair[1][i];
+					elem.value = pair[1][i][0];
+					elem.checked = pair[1][i][1];
+					if (elem.checked && elem.type == 'radio'){
+						elem.click();
+					}
 				}else{
 					elem.value = pair[1][i];
 				}
